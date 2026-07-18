9cd70607c6afd0ce75b779c6118fd111bf384ce6
Nagaveni <nagaveninagashetty21@gmail.com>
2026-07-18 14:10:27 +0530

    completed follow unfollow apis

---
+++ src/routes/user.routes.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const userController = require("../controllers/user.controller")
+const {identifyUser} = require("../middlewares/auth.middleware")
+const userRouter = express.Router();
+
+
+/**
+ * @Route POST /api/users/follow/:userid
+ * @description Follow a user
+ * @access Private
+ */
+userRouter.post("/follow/:username",identifyUser,userController.followUserController)
+
+
+
+/**
+ * @Route POST /api/users/unfollow/:userid
+ * @description unFollow a user
+ * @access Private
+ */
+userRouter.post("/unfollow/:username",identifyUser,userController.unfollowUserController)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = userRouter
