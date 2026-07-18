7072f3b26c863a578a80ae7a32e7176fbe2a9cb6
Nagaveni <nagaveninagashetty21@gmail.com>
2026-07-18 11:26:27 +0530

    completed the Get post and Get post details apis feature

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -50,10 +50,87 @@ try{
 }
 
 
+async function getPostController(req, res) {
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message : "Unauthorized access"
+        })
+    }
+
+    let decoded;
+    try {
+         decoded = jwt.verify(token , process.env.JWT_SECRET)
+    }catch(err) {
+        return res.status(401).json({
+            message:"Token invalid"
+        })
+    }
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user: userId
+    })
+    
+    res.status(200).json({
+        message : "post fetched successfully.",
+        posts
+    })
+}
+
+
+
+async function getPostDetailsController(req , res) {
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message : "Unauthorized access."
+        })
+    }
+    
+    let decoded ;
+    try {
+        decoded = jwt.verify(token , process.env.JWT_SECRET);
+    }catch(err) {
+        return res.status(401).json({
+            message : "Invalid token."
+        })
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post) {
+        return res.status(404).json({
+            message : "Post not found."
+        })
+    }
+
+    // const isValidUser = post.user.equals(userId ) ;
+    const isValidUser = post.user.toString() ===userId;
+    if(!isValidUser){
+        res.status(403).json({
+            message : "Forbidden content."
+        })
+    }
+
+    return res.status(200).json({
+        message : "Post fetched successfully",
+        post
+    })
+
+}
+
 
 
 
 
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -14,4 +14,23 @@ const upload = multer({storage:multer.memoryStorage()})
 /*  /api/posts/ */
 postRouter.post("/",upload.single("image"), postController.createPostController)
 
+/**
+ * GET /api/posts/ [protected]
+ */
+postRouter.get("/",postController.getPostController);
+
+/**
+ * GET /api/posts/details/:postid
+ * - return detail about specific post with the id. also check whether the post
+ * belongs to the user that is request come from 
+ */
+
+postRouter.get("/details/:postId",postController.getPostDetailsController)
+
+
+
+
+
+
+
 module.exports = postRouter
